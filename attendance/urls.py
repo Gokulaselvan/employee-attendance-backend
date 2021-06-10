aecbf3200bf6ac54673abceab776894dcdfcf5cd
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-10 20:28:04 +0530

    Added api login in browsable api page

--- attendance/urls.py
+++ attendance/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
