b8dfd3a8059fce9b583b8b7a37e4d30d3d967138
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-12 21:26:42 +0530

    Created User Profiles Model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields import DateField
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserProfile(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Need to change it as foreign key field
+    designation = models.CharField(max_length=50)
+
+    # Need to change it as foreign key field
+    department = models.CharField(max_length=50)
+    # Need to add Profile picture field
+
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+    joining_date = models.DateField(auto_now=False, auto_now_add=False)
+    experience = models.IntegerField()
+    shift = models.CharField(max_length=50)
+
+    about = models.TextField()
+
+    def __str__(self) -> str:
+        return self.user.email
