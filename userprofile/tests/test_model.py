492418b1ed9887738a79f330db6a5c9727e98815
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-12 22:44:10 +0530

    Added test for user exp is +ve

--- userprofile/tests/test_model.py
+++ userprofile/tests/test_model.py
@@ -10,10 +10,21 @@ class TestUserProfileModel(TestCase):
     def setUp(self) -> None:
 
         self.userprofile = baker.make(UserProfile)
+        self.userprofile1 = baker.make(UserProfile, experience=2)
         return super().setUp()
 
     def test_create_usercount(self) -> None:
+        """Checks the User count as per baker"""
 
         user_count = UserProfile.objects.all().count()
 
-        self.assertEqual(user_count, 1)
+        self.assertEqual(user_count, 2)
+
+    def test_check_experience_is_postitve_num(self) -> None:
+        """Checks the User experience is positive integer only"""
+
+        user_exp = UserProfile.objects.get(id=self.userprofile.id)
+        user_exp1 = UserProfile.objects.get(id=self.userprofile1.id)
+
+        self.assertTrue(user_exp.experience == 0)
+        self.assertTrue(user_exp1.experience > 0)
