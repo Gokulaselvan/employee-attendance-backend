f46ea6eaf99314ab9cf77c6640aa58761a24e980
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-13 17:38:18 +0530

    Added validators for experience and joining date

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.db.models.fields import PositiveIntegerField
+
+from django.core.validators import MaxValueValidator
 
 User = get_user_model()
 
@@ -18,7 +20,9 @@ class UserProfile(models.Model):
 
     date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
     joining_date = models.DateField(auto_now=False, auto_now_add=False)
-    experience = models.PositiveIntegerField(default=0)
+
+    experience = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(limit_value=99)])
 
     # Need to change it as a foreign field
     shift = models.CharField(max_length=50)
@@ -27,3 +31,9 @@ class UserProfile(models.Model):
 
     def __str__(self) -> str:
         return self.user.email
+
+    def clean(self) -> None:
+        if self.date_of_birth > self.joining_date:
+            raise ValidationError(
+                "Date Of Birth must be lesser than the Joining Date")
+        return super().clean()
