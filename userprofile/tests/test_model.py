f34b368ac18a7e341db1cadc46ad43419d274a4c
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-14 22:09:51 +0530

    Added two more tests for userprofile

--- userprofile/tests/test_model.py
+++ userprofile/tests/test_model.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from model_bakery import baker
 
@@ -11,6 +12,8 @@ class TestUserProfileModel(TestCase):
 
         self.userprofile = baker.make(UserProfile)
         self.userprofile1 = baker.make(UserProfile, experience=2)
+        self.userprofile2 = baker.make(
+            UserProfile, date_of_birth="2000-12-12", joining_date="1999-12-12")
         return super().setUp()
 
     def test_create_usercount(self) -> None:
@@ -18,7 +21,7 @@ class TestUserProfileModel(TestCase):
 
         user_count = UserProfile.objects.all().count()
 
-        self.assertEqual(user_count, 2)
+        self.assertEqual(user_count, 3)
 
     def test_check_experience_is_postitve_num(self) -> None:
         """Checks the User experience is positive integer only"""
@@ -28,3 +31,16 @@ class TestUserProfileModel(TestCase):
 
         self.assertTrue(user_exp.experience == 0)
         self.assertTrue(user_exp1.experience > 0)
+
+    def test_str_repr_model(self) -> None:
+        """Checks for the Str method"""
+
+        user_str = UserProfile.objects.get(id=self.userprofile.id)
+
+        self.assertEqual(str(user_str), self.userprofile.user.email)
+
+    def test_create_user_dob_lt_jd(self) -> None:
+        """Checks for DOB less than Joining Date"""
+
+        with self.assertRaises(ValidationError):
+            self.userprofile2.full_clean()
