f22d268372b144a9b6d30e141095d989b5dc4b8e
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-15 08:50:54 +0530

    Added tests for user creation form

--- user/forms.py
+++ user/forms.py
@@ -27,7 +27,7 @@ class CustomUserCreationForm(forms.ModelForm):
         retype_password = cleaned_data.get("retype_password")
 
         if password is not None and retype_password is not None and password != retype_password:
-            self.add_error("password_2", "Your passwords must match")
+            self.add_error("retype_password", "Your passwords must match")
         return cleaned_data
 
     def save(self, commit=True):
