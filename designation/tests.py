d02c97722e720cd9cb5d52f73ae66af70d33f05b
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-17 20:56:55 +0530

    Added str test for Department

--- designation/tests.py
+++ designation/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Designation
+from .models import Designation, Department
 
 
 class TestDesignation(TestCase):
@@ -8,6 +8,17 @@ class TestDesignation(TestCase):
     def test_str_repr(self) -> None:
         """Checks for the str method"""
 
-        designation = Designation.objects.create(designation_name = "Product Manager")
+        designation = Designation.objects.create(
+            designation_name="Product Manager")
 
         self.assertEqual(str(designation), "Product Manager")
+
+
+class TestDepartment(TestCase):
+
+    def test_str_repr(self) -> None:
+        """Checks for the str method"""
+
+        department = Department.objects.create(department_name="Development")
+
+        self.assertEqual(str(department), "Development")
