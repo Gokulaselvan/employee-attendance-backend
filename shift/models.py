9411d4016d644d69eccd31a522ad09fc9d1eea55
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-20 14:35:39 +0530

    Added shift model

--- shift/models.py
+++ shift/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Shift(models.Model):
+
+    shift_name = models.CharField(max_length=50)
+    shift_timing_start = models.TimeField(auto_now=False, auto_now_add=False)
+    shift_timing_end = models.TimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+
+        return self.shift_name
