b9495a8f261ce032b8fa961b0798ba374f4cd91d
gokulaselvan <gokul.erdesign@gmail.com>
2021-06-20 14:56:31 +0530

    Added tests for shift str

--- shift/tests.py
+++ shift/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from .models import Shift
+
+
+class TestShift(TestCase):
+
+    def test_str_repr(self) -> None:
+
+        shift = Shift.objects.create(
+            shift_name="General",
+            shift_timing_start=datetime.time(9, 00, 00),
+            shift_timing_end=datetime.time(5, 30, 00),
+        )
+        self.assertEqual(str(shift), "General")
